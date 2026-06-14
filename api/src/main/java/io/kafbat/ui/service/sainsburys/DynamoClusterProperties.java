a070b9643a5c9159a86c543f0fc4ba51ecbc5512
nkululekog <nkululeko.gininda@sainsburys.co.uk>
2026-06-14 11:41:01 +0200

    feat: skip loading masing config for dev testing purposes

--- api/src/main/java/io/kafbat/ui/service/sainsburys/DynamoClusterProperties.java
+++ api/src/main/java/io/kafbat/ui/service/sainsburys/DynamoClusterProperties.java
@@ -12,7 +12,6 @@ import io.kafbat.ui.repository.DynamoMaskingEntityRepository;
 import io.kafbat.ui.repository.DynamoRbacEntityRepository;
 import io.kafbat.ui.service.masking.DataMasking;
 import io.kafbat.ui.service.masking.policies.MaskingPolicy;
-import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -30,11 +29,6 @@ public class DynamoClusterProperties {
   private final DynamoMaskingEntityRepository maskingEntityRepository;
   private final DynamoRbacEntityRepository rbacEntityRepository;
   private List<DynamoMaskingEntity> maskingEntityList;
-
-  @PostConstruct
-  public void loadConfiguration() {
-//    this.maskingEntityList = maskingEntityRepository.findAll();
-  }
 
   public void loadMaskingConfiguration() {
     this.maskingEntityList = maskingEntityRepository.findAll();
