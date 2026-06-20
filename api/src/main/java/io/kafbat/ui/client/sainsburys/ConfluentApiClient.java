1605985b21649a36b1aa1afb20a2626db9a305ec
nkululekog <nkululeko.gininda@sainsburys.co.uk>
2026-06-20 23:42:09 +0200

    feat: update mask config retrieve

--- api/src/main/java/io/kafbat/ui/client/sainsburys/ConfluentApiClient.java
+++ api/src/main/java/io/kafbat/ui/client/sainsburys/ConfluentApiClient.java
@@ -14,14 +14,13 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "confluent-api-client",
-    url = "${sainsburys.external.services.confluent-api.base-url}",
     configuration = { HttpFeignConfig.class })
 public interface ConfluentApiClient {
 
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.tagdefs}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<List<TagDefinitionClassificationResponse>> retrieveTagDefinitions(URI baseUrl,
                                               @RequestHeader("Authorization") String authorization);
@@ -29,7 +28,7 @@ public interface ConfluentApiClient {
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.topic}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<SchemaMetadataResponse> retrieveTopicMetadata(URI baseUrl,
                                             @RequestHeader("Authorization") String authorization,
@@ -38,7 +37,7 @@ public interface ConfluentApiClient {
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.topic}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<SchemaMetadataResponse> retrieveTopicMetadata(URI baseUrl,
                                                                @RequestHeader("Authorization") String authorization);
@@ -46,7 +45,7 @@ public interface ConfluentApiClient {
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.subjects}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<List<String>> retrieveTopicList(URI baseUrl,
                                                                @RequestHeader("Authorization") String authorization);
@@ -54,7 +53,7 @@ public interface ConfluentApiClient {
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.topic.fields}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<SchemaMetadataResponse> retrieveTopicFieldsMetadata(URI baseUrl,
                                         @RequestHeader("Authorization") String authorization,
@@ -63,7 +62,7 @@ public interface ConfluentApiClient {
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.schema}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<SchemaMetadataResponse> retrieveSchemaMetadata(URI baseUrl,
                                                                 @RequestHeader("Authorization") String authorization);
@@ -71,7 +70,7 @@ public interface ConfluentApiClient {
   @GetMapping(path = "${sainsburys.external.services.confluent-api.operations.retrieve.subject}",
       headers = {
         "Content-Type=application/vnd.schemaregistry.v1+json",
-        "Accept=application/vnd.schemaregistry.v1+json"
+        "Accept=*/*"
       })
   ResponseEntity<SubjectMetadataResponse> retrieveSubjectMetadata(URI baseUrl,
                                                                   @RequestHeader("Authorization") String authorization,
