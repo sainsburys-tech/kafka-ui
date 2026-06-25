26218cb34b36dcb4d59dfa285ce4c1e1c89113b8
nkululekog <nkululeko.gininda@sainsburys.co.uk>
2026-06-25 12:57:30 +0200

    feat: update UnmaskingController add masking flag check

--- api/src/main/java/io/kafbat/ui/controller/sainsburys/UnmaskingController.java
+++ api/src/main/java/io/kafbat/ui/controller/sainsburys/UnmaskingController.java
@@ -4,6 +4,7 @@ import static io.kafbat.ui.model.rbac.permission.TopicAction.MESSAGES_READ;
 
 import io.kafbat.ui.api.UnmaskingApi;
 import io.kafbat.ui.controller.AbstractController;
+import io.kafbat.ui.exception.ValidationException;
 import io.kafbat.ui.model.UnmaskRequestDTO;
 import io.kafbat.ui.model.rbac.AccessContext;
 import io.kafbat.ui.service.mcp.McpTool;
@@ -11,6 +12,7 @@ import io.kafbat.ui.service.sainsburys.UnmaskingService;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +26,18 @@ public class UnmaskingController extends AbstractController implements Unmasking
 
   private final UnmaskingService unmaskingService;
 
+  @Value("${sainsburys.masking.feature.enabled: 'false' }")
+  private String isMaskingEnabled;
 
   @Override
   public Mono<ResponseEntity<Void>> dataUnmasking(String clusterName, String topicName,
                                                   Mono<UnmaskRequestDTO> unmaskRequestDto, ServerWebExchange exchange) {
     log.info("Unmasking cluster {}, topic {} messages", clusterName, topicName);
+
+    if (!Boolean.parseBoolean(isMaskingEnabled)) {
+      throw new ValidationException("Data masking feature is not in active status.");
+    }
+
     return exchange.getPrincipal()
         .map(Principal::getName)
         .flatMap(principal -> {
